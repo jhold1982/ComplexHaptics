d0a5bce330c870f3a14dd589fac4d3c2bae2b1d3
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2024-04-22 16:24:52 -0500

    Initial Commit

---
+++ ComplexHaptics/ComplexHapticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComplexHapticsApp.swift
+//  ComplexHaptics
+//
+//  Created by Justin Hold on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct ComplexHapticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
